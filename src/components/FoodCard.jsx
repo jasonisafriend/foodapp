cdfb6496dfab66516311719a8f7579b7f555255a
Jason Mei <jason@jasonmei.com>
2026-04-13 21:17:27 -0400

    Update Action-Go icon

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -107,8 +107,9 @@ function LocationGoAction({ food }) {
       style={glassStyle}
       aria-label={`Open ${food.location} in Maps`}
     >
-      <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="black" strokeWidth={1.5}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M7 17L17 7M17 7H8M17 7v9" />
+      {/* Phosphor ArrowUpRight (regular) */}
+      <svg className="w-6 h-6" fill="black" viewBox="0 0 256 256" aria-hidden="true">
+        <path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z" />
       </svg>
     </button>
   )
